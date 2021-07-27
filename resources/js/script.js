552f1f60d7532f625b0817fe59e452af186eb47c
倉林智也 <kurahayashitomoya@kurabayashitomoyanoMacBook-Pro.local>
2021-07-27 20:40:39 +0900

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
---
+++ resources/views/home.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+   <link href="{('../../public/css/style.css')}" rel="stylesheet">
+  <script src="../js/script.js" defer></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+  <title>Document</title>
+</head>
+<body style="background-image: url(../../public/img/back.jpg)">
+  <header>
+    <div class="info">
+    <h1>わかば幼稚園</h1>
+      <p class="info-text1">アクセス</p>
+      <p class="info-text2">お問い合わせ</p>
+    </div>
+
+    <nav>
+      <ul>
+        <li>当園について</li>
+        <li>園生活について</li>
+        <li>入園案内</li>
+        <li>預かり保育</li>
+        <li>採用情報</li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <slider class="slider">
+      <img class="back-img" src="../../public/img/blur-img.png">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide"><img src="../../public/img/slide2.jpeg" alt=""></div>
+          <div class="swiper-slide"><img src="../../public/img/slide1.jpg" alt=""></div>
+          <div class="swiper-slide"><img src="../../public/img/slide3.jpeg" alt=""></div>
+        </div>
+
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+     </div>
+    </slider>
+
+      <div class="announce">
+        <h2>お知らせ</h2>
+      </div>
+
+    <section class="about">
+      <div class="about-text">
+        <h2>当園について</h2>
+        <p>当園では子どもの主体性を大切にしております。</p>
+      </div>
+      <img class="main-img" src="../../public/img/about.jpeg" alt="">
+    </section>
+
+
+  </main>
+</body>
+</html>
